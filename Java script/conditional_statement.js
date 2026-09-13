5cd8e324b2dd6f902863c04f6024cd606b616665
Ritesh.kr <ritesh9798823@gmail.com>
2026-09-13 10:53:30 +0530

    conditional statements

---
+++ Java script/conditional_statement.js
@@ -0,0 +1,21 @@
+
+
+// cheacking if string is equal to another string.
+
+
+let user1 = "String piyo ge";
+let user2 = "String piyo ge";
+let user3 = "Kaam kar le";
+
+
+if (user1 == user2) {
+    console.log("Ji ye dono to string ke divane hai");
+}
+else if (user1 == user3) {
+
+    console.log("Ji kuch nahi ho sakta aap ka");
+}
+
+else {
+    "Ghee khatam";
+}
