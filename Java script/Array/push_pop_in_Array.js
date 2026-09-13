2a48f3f2020fe3f8cac62661c477686be7744115
Ritesh.kr <ritesh9798823@gmail.com>
2026-09-13 11:38:07 +0530

    Push And pop in Array

---
+++ Java script/Array/push_pop_in_Array.js
@@ -0,0 +1,14 @@
+
+let City_visited = ["Noida", "New-delhi", "Mumbai"];
+City_visited.push("Kolkata");
+
+console.log(City_visited);
+
+let tea_orders = ["Earl grey", "chai", "Ginger_tea", "matcha", "Turmeric_latte"];
+
+tea_orders.pop();
+
+last_order = tea_orders.pop();
+
+console.log(tea_orders);
+console.log("... -->" + last_order);
