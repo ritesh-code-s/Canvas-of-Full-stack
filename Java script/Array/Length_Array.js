ee249213945ad635e9a075dd60a64896a2a27b06
Ritesh.kr <ritesh9798823@gmail.com>
2026-09-13 12:05:41 +0530

    Merge two Array

---
+++ Java script/Array/Length_Array.js
@@ -0,0 +1,9 @@
+
+// length of An Array
+
+
+let fav_char_in_MCU = ["Thor", "Iron-Man", "Spider-man", "Dr-strange", "Wanda"];
+
+let length_of_Array = fav_char_in_MCU.length;
+
+console.log(length_of_Array);
